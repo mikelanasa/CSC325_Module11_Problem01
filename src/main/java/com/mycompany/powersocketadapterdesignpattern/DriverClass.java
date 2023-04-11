5edf475a900d5641fb1f247f1a7b71bd4bf5f1aa
mikelanasa <mikelanasa@gmail.com>
2023-04-10 21:04:51 -0400

    Finished classes and methods

--- src/main/java/com/mycompany/powersocketadapterdesignpattern/DriverClass.java
+++ src/main/java/com/mycompany/powersocketadapterdesignpattern/DriverClass.java
@@ -5,12 +5,19 @@
 package com.mycompany.powersocketadapterdesignpattern;
 
 /**
- *
+ * Class to hold the main method
  * @author mikel
  */
 public class DriverClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Demonstrate classes and methods
+        AmericanPowerPlug plug1 = new AmericanAppliance();
+        
+        EuropeanPowerSocket socket1 = new EuropeanPowerSocket();
+        
+        EuropeanPowerPlug plug2 = new AmericanToEuropeanAdapter(plug1);
+        
+        socket1.plugInAppliance(plug2);
     } // ends main
 } // ends class
